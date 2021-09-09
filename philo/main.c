73ac9043d30adc33fc1a71e0ba98cb3da45c83cf
ericard42 <ericard@student.42.fr>
2021-09-09 20:49:37 +0200

    oui

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: ericard <ericard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 17:22:53 by ericard           #+#    #+#             */
-/*   Updated: 2021/08/05 14:29:10 by ericard          ###   ########.fr       */
+/*   Updated: 2021/08/31 09:44:44 by ericard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		main(int ac, char **av)
 
 	if (ac < 5 || ac > 6)
 		return (ft_error(ARGUMENT));
-	if (ret = init(&philo, ac, av) != 0)
+	if (ret == init(&philo, ac, av) != 0)
 		return (ret);
 	return (1);
 }
