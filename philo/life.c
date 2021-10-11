8993edd3628a6ac6ea52cc4cf77f2f8d9452c243
Emily FLYNN Elodie RICARD <ericard@student.42.fr>
2021-10-11 18:30:43 +0200

    fix leaks

--- philo/life.c
+++ philo/life.c
@@ -6,7 +6,7 @@
 /*   By: ericard <ericard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 16:11:47 by ericard@stu       #+#    #+#             */
-/*   Updated: 2021/10/11 17:32:54 by ericard          ###   ########.fr       */
+/*   Updated: 2021/10/11 18:30:23 by ericard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ void *routine(void *p_data)
 {
 	t_philo		*philo;
 	pthread_t	dead;
+	pthread_t	dead2;
 	int i = 1;
 	
 	philo = (t_philo *)p_data;
-	pthread_create(&dead, NULL, &time_to_die, philo);
+	dead = 0;
+	pthread_create(&dead2, NULL, &time_to_die, philo);
 	while (42)
 	{
 		if (i == 1 && philo->frame->num_philos >= 2 && (philo->position % 2) != 0)
@@ -28,7 +30,9 @@ void *routine(void *p_data)
 		if (philo->frame->philo_die == 1)
 		{
 			drop_fork(philo);
-			pthread_join(dead, NULL);
+			if (dead)
+				pthread_join(dead, NULL);
+			pthread_join(dead2, NULL);
 			return NULL;			
 		}
 		philo->last_time_eat = g_time();
@@ -39,12 +43,14 @@ void *routine(void *p_data)
 		if (philo->frame->philo_die == 1)
 		{
 			pthread_join(dead, NULL);
+			pthread_join(dead2, NULL);
 			return NULL;			
 		}
 		if (philo->frame->num_eat != -1 && i == philo->frame->num_eat)
 		{
 			philo->last_eat = 1;
 			pthread_join(dead, NULL);
+			pthread_join(dead2, NULL);
 			return NULL;			
 		}
 		print_sleep(philo->position, philo->frame);
@@ -52,16 +58,19 @@ void *routine(void *p_data)
 		if (philo->frame->philo_die == 1)
 		{
 			pthread_join(dead, NULL);
+			pthread_join(dead2, NULL);
 			return NULL;			
 		}
 		print_think(philo->position, philo->frame);
 		if (philo->frame->philo_die == 1)
 		{
 			pthread_join(dead, NULL);
+			pthread_join(dead2, NULL);
 			return NULL;			
 		}
 		i++;
 	}
 	pthread_join(dead, NULL);
+	pthread_join(dead2, NULL);
 	return NULL;
 }
