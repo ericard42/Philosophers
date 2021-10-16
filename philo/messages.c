444c8ab2a698c4651089e4a4efdf094a4f72da83
ericard42 <ericard@student.42.fr>
2021-10-16 10:33:16 +0200

    comment time_now for debug

--- philo/messages.c
+++ philo/messages.c
@@ -5,7 +5,8 @@ void	print_fork(int nb_philo, t_frame *frame)
 	unsigned int	time_now;
 
 	pthread_mutex_lock(&frame->message);
-	time_now = g_time() - frame->philos[nb_philo - 1].last_time_eat;
+	//time_now = g_time() - frame->philos[nb_philo - 1].last_time_eat;
+	time_now = g_time() - frame->start_time;
 	printf("%d\t%d has taken a fork\n", time_now, nb_philo);
 	pthread_mutex_unlock(&frame->message);
 }
@@ -15,7 +16,8 @@ void	print_eat(int nb_philo, t_frame *frame)
 	unsigned int	time_now;
 
 	pthread_mutex_lock(&frame->message);
-	time_now = g_time() - frame->philos[nb_philo - 1].last_time_eat;
+	//time_now = g_time() - frame->philos[nb_philo - 1].last_time_eat;
+	time_now = g_time() - frame->start_time;
 	printf("%d\t%d is eating\n", time_now, nb_philo);
 	pthread_mutex_unlock(&frame->message);
 }
@@ -25,7 +27,8 @@ void	print_sleep(int nb_philo, t_frame *frame)
 	unsigned int	time_now;
 
 	pthread_mutex_lock(&frame->message);
-	time_now = g_time() - frame->philos[nb_philo - 1].last_time_eat;
+	//time_now = g_time() - frame->philos[nb_philo - 1].last_time_eat;
+	time_now = g_time() - frame->start_time;
 	printf("%d\t%d is sleeping\n", time_now, nb_philo);
 	pthread_mutex_unlock(&frame->message);
 }
@@ -35,7 +38,8 @@ void	print_think(int nb_philo, t_frame *frame)
 	unsigned int	time_now;
 
 	pthread_mutex_lock(&frame->message);
-	time_now = g_time() - frame->philos[nb_philo - 1].last_time_eat;
+	//time_now = g_time() - frame->philos[nb_philo - 1].last_time_eat;
+	time_now = g_time() - frame->start_time;
 	printf("%d\t%d is thinking\n", time_now, nb_philo);
 	pthread_mutex_unlock(&frame->message);
 }
@@ -45,7 +49,8 @@ void	print_died(int nb_philo, t_frame *frame)
 	unsigned int	time_now;
 
 	pthread_mutex_lock(&frame->message);
-	time_now = g_time() - frame->philos[nb_philo - 1].last_time_eat;
+	//time_now = g_time() - frame->philos[nb_philo - 1].last_time_eat;
+	time_now = g_time() - frame->start_time;
 	printf("%d\t%d died\n", time_now, nb_philo);
 	pthread_mutex_unlock(&frame->message);
 }
